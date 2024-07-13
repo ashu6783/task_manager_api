296c3e069b949629143d0c3b87f880ad3c96a3d3
Ashutosh Gaurav <123999741+ashu6783@users.noreply.github.com>
2024-07-14 00:52:59 +0530

    running

--- starter/app.js
+++ starter/app.js
@@ -6,12 +6,13 @@ const connectDB= require('./db/connect')
 require('dotenv').config()
 //middleware
 
+app.use(express.static('./public'))
 app.use(express.json())
 
+
+
 //routes
-app.get('/hello',(req,res)=>{
-    res.send('task manager app')
-})
+
 app.use('/api/v1/tasks',tasks)
 
 const port =3000
